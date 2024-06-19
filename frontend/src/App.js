f2cdb9e6413c8c9f73de333eb7a5427fcc5cb6aa
roylimxinwei <102473862+roylimxinwei@users.noreply.github.com>
2024-06-19 20:18:37 +0800

    "Initial commit"

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import DataComponent from "./components/DataComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My Passion Project</h1>
+        <DataComponent />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/DataComponent.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from "react";
+
+const DataComponent = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/data")
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
+  return (
+    <div>
+      <h1>Data from Backend</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
+};
+
+export default DataComponent;
